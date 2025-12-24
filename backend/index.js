46977fac7f99116726ad1e5a9abb6f4f5d24aef9
PratikKOlhe-02 <pratikkolhe02.official@gmail.com>
2025-12-25 00:52:51 +0530

    Prepairing backend for production CORS

--- backend/index.js
+++ backend/index.js
@@ -17,8 +17,8 @@ let app = express()
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-    origin:"http://localhost:5173",
-    credentials:true
+    origin: process.env.FRONTEND_URL, 
+    credentials: true
 }))
 app.use("/api/auth", authRouter)
 app.use("/api/user", userRouter)
